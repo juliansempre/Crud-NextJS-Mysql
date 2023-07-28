d307d00edb72e30f12e9e1b285a66988c627e457
Julian Silva <31382024+juliansempre@users.noreply.github.com>
2023-07-27 21:53:38 -0300

    Update db.ts

--- pages/api/model/db.ts
+++ pages/api/model/db.ts
@@ -1,10 +1,26 @@
-import {createPool} from 'mysql2/promise';
+//POSTGRES
 
-const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    database: 'nextjs'
-})
+import { Pool } from 'pg';
 
-export {pool};
+const pool = new Pool({
+  host: 'localhost',
+  user: 'seu_usuario',
+  password: 'sua_senha',
+  port: 5432, // Porta padrão do PostgreSQL
+  database: 'nextjs',
+});
+
+export { pool };
+
+
+//MYSQL
+// import {createPool} from 'mysql2/promise';
+
+// const pool = createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     port: 3306,
+//     database: 'nextjs'
+// })
+
+// export {pool};

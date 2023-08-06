db356cf8a819f72d902571b14a5d84138eff2252
Julian Silva <31382024+juliansempre@users.noreply.github.com>
2023-08-05 21:05:01 -0300

    Update db.ts

--- pages/api/model/db.ts
+++ pages/api/model/db.ts
@@ -1,26 +1,27 @@
 //POSTGRES
 
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
-const pool = new Pool({
-  host: 'localhost',
-  user: 'seu_usuario',
-  password: 'sua_senha',
-  port: 5432, // Porta padrão do PostgreSQL
-  database: 'nextjs',
-});
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'seu_usuario',
+//   password: 'sua_senha',
+//   port: 5432, // Porta padrão do PostgreSQL
+//   database: 'nextjs',
+// });
 
-export { pool };
+// export { pool };
 
 
-//MYSQL
-// import {createPool} from 'mysql2/promise';
+MYSQL
+import {createPool} from 'mysql2/promise';
 
-// const pool = createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     port: 3306,
-//     database: 'nextjs'
-// })
+const pool = createPool({
+    host: '85.10.205.173',
+    user: 'rootportfolio1',
+    password: 'toor123@',
+    port: 3306,
+    database: 'bancojulian1'
+})
 
-// export {pool};
+export {pool};
